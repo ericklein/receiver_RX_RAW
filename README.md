687dafae2c92cc32ece634754d7a420e77786357
sircoolio <ericklein@users.noreply.github.com>
2020-04-06 22:00:32 -0700

    v2
    code cleanup and conditional debug messages

--- README.md
+++ README.md
@@ -1,19 +1,50 @@
 # receiver
-900mhz receiver
+## What is receiver? 
+Simple packet communication between 900mhz radios. receiver is half of this solution, the other half is the transmitter application.
 
-Description : Simple communication between 900mhz radios
+### Purpose
+Validate simple package transmission and reception for inclusion in other solutions
 
-Sources
-  - uses Radiohead https://github.com/PaulStoffregen/RadioHead
-    
-  Target
-    - Works with all Arduino boards, uses hardware SPI ports which are board dependent
-    - Overview of Adafruit RFM69HCW at: https://www.adafruit.com/product/3070
-    
-  Revisions
-    010318
-      - first version, adapted from adapted from RadioHead69_RawDemo_RX
+### Software Dependencies
+  - uses Radiohead http://www.airspayce.com/mikem/arduino/RadioHead/RadioHead-1.100.zip
+    - adfruit maintains a GitHub branch at https://github.com/adafruit/RadioHead
+  - uses Adafruit_LiquidCrystal https://github.com/adafruit/Adafruit_LiquidCrystal
 
-  Feature Requests
-    010318
-      - xxx
+### BOM
+  - 1X: Adafruit Arduino Pro Mini 328 - 5V (part 2378) 
+  - 1X: Adafruit FRM69HCW Radio Breakout (part 3070)
+  - 1X: Adafruit FTDI Friend (part 284)
+  - [optional]1X: 16x2 LCD panel
+  - [optional]1X: Adafruit i2c character LCD backpack (part 292)
+  - 1 protoboard
+  - wire
+
+### Pinouts
+  - see Fritzing diagram
+  - Pro A4 (SDA) for LCD backpack
+  - Pro A5 (SLC) for LCD backpack
+
+### Information Sources
+    - Adafruit RFM69HCW (SPI): https://learn.adafruit.com/adafruit-rfm69hcw-and-rfm96-rfm95-rfm98-lora-packet-padio-breakouts?view=all
+    - Sparkfun RFM69HCW: https://learn.sparkfun.com/tutorials/rfm69hcw-hookup-guide/all
+    - Adafruit i2c/SPI LCD Backpack: https://learn.adafruit.com/i2c-spi-lcd-backpack/
+    - Radiohead main page: https://www.airspayce.com/mikem/arduino/RadioHead/index.html
+    - 16x2 LCD panels: http://oomlout.com/parts/LCDD-01-guide.pdf
+
+### Issues
+  - 040620: displayMessage will leave visual artifacts if message string decreases in size
+
+### Questions
+
+
+### Learnings
+
+### Feature Requests
+
+### Revisions *(independent of check-ins)*
+    - 010318: first version, adapted from adapted from RadioHead69_RawDemo_RX
+    - 010318: added LCD interface for non-serial monitor output
+    - 040620: code cleanup
+    - 040620: [FR 010318] move serial debug code to conditional compile
+    - 040620: [FR 010318] display error conditions on LCD display
+    - 040620: Github checkin
--- receiver_RX_RAW.ino
+++ receiver_RX_RAW.ino
@@ -6,129 +6,167 @@
   See README.md for target information, revision history, feature requests, etc.
 */
 
+#define DEBUG       // Output to the serial port
+#define EXT_DEBUG   // Output to external LCD display
+
 #include <SPI.h>
 #include <RH_RF69.h>
 
-/************ Radio Setup ***************/
-
-// Change to 434.0 or other frequency, must match RX's freq!
-#define RF69_FREQ 915.0
-
-#if defined (__AVR_ATmega32U4__) // Feather 32u4 w/Radio
-  #define RFM69_CS      8
-  #define RFM69_INT     7
-  #define RFM69_RST     4
+#ifdef EXT_DEBUG
+  #include <Adafruit_LiquidCrystal.h>
+  #include <Wire.h>
+  // LCD connection via i2c, default address #0 (A0-A2 not jumpered)
+  Adafruit_LiquidCrystal lcd(0);
 #endif
 
-#if defined(ARDUINO_SAMD_FEATHER_M0) // Feather M0 w/Radio
+// Radio setup
+// 400-[433]-600 for LoRa radio, 850-[868,915]-950 for 900mhz, must match transmitter
+#define RF69_FREQ 915.0
+
+// Conditional code compiles
+
+#if defined(ARDUINO_SAMD_FEATHER_M0)
   #define RFM69_CS      8
   #define RFM69_INT     3
   #define RFM69_RST     4
 #endif
 
-#if defined (__AVR_ATmega328P__)  // Feather 328P w/wing
+#if defined (__AVR_ATmega328P__)
   #define RFM69_INT     3  // 
   #define RFM69_CS      4  //
   #define RFM69_RST     2  // "A"
 #endif
 
-#if defined(ESP8266)    // ESP8266 feather w/wing
-  #define RFM69_CS      2    // "E"
-  #define RFM69_IRQ     15   // "B"
-  #define RFM69_RST     16   // "D"
-#endif
-
-#if defined(ESP32)    // ESP32 feather w/wing
-  #define RFM69_RST     13   // same as LED
-  #define RFM69_CS      33   // "B"
-  #define RFM69_INT     27   // "A"
-#endif
-
-/* Teensy 3.x w/wing
-#define RFM69_RST     9   // "A"
-#define RFM69_CS      10   // "B"
-#define RFM69_IRQ     4    // "C"
-#define RFM69_IRQN    digitalPinToInterrupt(RFM69_IRQ )
-*/
- 
-/* WICED Feather w/wing 
-#define RFM69_RST     PA4     // "A"
-#define RFM69_CS      PB4     // "B"
-#define RFM69_IRQ     PA15    // "C"
-#define RFM69_IRQN    RFM69_IRQ
-*/
-
-// Singleton instance of the radio driver
+// Instantiate radio driver
 RH_RF69 rf69(RFM69_CS, RFM69_INT);
 
-int16_t packetnum = 0;  // packet counter, we increment per xmission
+// packet counter, incremented per transmission
+int16_t packetnum = 0;
 
 void setup() 
 {
-  Serial.begin(115200);
-  //while (!Serial) { delay(1); } // wait until serial console is open, remove if not tethered to computer
+  #ifdef DEBUG
+    Serial.begin(115200);
+    while (!Serial) ;
+  #endif
 
+  #ifdef EXT_DEBUG
+    //LCD setup
+    lcd.begin(16, 2);
+    lcd.setBacklight(HIGH);
+  #endif
+
+  // radio setup
   pinMode(RFM69_RST, OUTPUT);
   digitalWrite(RFM69_RST, LOW);
 
-  Serial.println("Feather RFM69 RX Test!");
-  Serial.println();
-
-  // manual reset
+   // radio reset
   digitalWrite(RFM69_RST, HIGH);
   delay(10);
   digitalWrite(RFM69_RST, LOW);
   delay(10);
   
-  if (!rf69.init()) {
-    Serial.println("RFM69 radio init failed");
+  if (!rf69.init())
+  {
+    #ifdef DEBUG
+      Serial.println("RFM69 radio init failed");
+    #endif
+    #ifdef EXT_DEBUG
+      displayMessage(0, "Radio init failed");
+    #endif
     while (1);
   }
-  Serial.println("RFM69 radio init OK!");
   
-  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM (for low power module)
-  // No encryption
-  if (!rf69.setFrequency(RF69_FREQ)) {
-    Serial.println("setFrequency failed");
+  // Default after radio init is 434.0MHz
+  if (!rf69.setFrequency(RF69_FREQ))
+  {
+    #ifdef DEBUG
+      Serial.println("RFM69 set frequency failed");
+    #endif
+    #ifdef EXT_DEBUG
+      displayMessage(0, "Can't set frequency");
+    #endif
+    while (1);
   }
+  
+  // Defaults after radio init are modulation GFSK_Rb250Fd250, +13dbM (for low power module), no encryption
+  // For RFM69HW, 14-20dbm for power, 2nd arg must be true for 69HCW
+  rf69.setTxPower(20, true);
 
-  // If you are using a high power RF69 eg RFM69HW, you *must* set a Tx power with the
-  // ishighpowermodule flag set like this:
-  rf69.setTxPower(20, true);  // range from 14-20 for power, 2nd arg must be true for 69HCW
-
-  // The encryption key has to be the same as the one in the server
+  // The encryption key has to match the transmitter
   uint8_t key[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
                     0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
   rf69.setEncryptionKey(key);
   
-  Serial.print("RFM69 radio @");  Serial.print((int)RF69_FREQ);  Serial.println(" MHz");
+  // radio successfully set up
+  #ifdef DEBUG
+    Serial.print("RFM69 radio OK @ ");
+    Serial.print((int)RF69_FREQ);
+    Serial.println("MHz. This node is receiver.");
+  #endif
+  #ifdef EXT_DEBUG
+    displayMessage(0, "Radio OK, =>receiver");
+    delay(2000);
+  #endif
 }
 
-
-void loop() {
- if (rf69.available()) {
+void loop() 
+{
+  if (rf69.available()) 
+  {
     // Should be a message for us now   
     uint8_t buf[RH_RF69_MAX_MESSAGE_LEN];
     uint8_t len = sizeof(buf);
-    if (rf69.recv(buf, &len)) {
+    if (rf69.recv(buf, &len))
+    {
       if (!len) return;
       buf[len] = 0;
-      Serial.print("Received [");
-      Serial.print(len);
-      Serial.print("]: ");
-      Serial.println((char*)buf);
-      Serial.print("RSSI: ");
-      Serial.println(rf69.lastRssi(), DEC);
 
-      if (strstr((char *)buf, "Hello World")) {
+      // display message
+      #ifdef DEBUG
+        Serial.print("Received: ");
+        Serial.println((char*)buf);
+        Serial.print("RSSI:");
+        Serial.println(rf69.lastRssi(), DEC);
+      #endif
+      #ifdef EXT_DEBUG
+        displayMessage(0, (char*)buf);
+        //displayMessage(1,(int) rf69.lastRssi());
+        String rssi_Message = "RSSI: ";
+        rssi_Message += rf69.lastRssi();
+        displayMessage(1, rssi_Message);
+        //displayMessage(1, (char*)rf69.lastRssi());
+      #endif
+
+      // respond to a specific message
+      if (strstr((char *)buf, "Hello World"))
+      {
         // Send a reply!
         uint8_t data[] = "And hello back to you";
         rf69.send(data, sizeof(data));
         rf69.waitPacketSent();
-        Serial.println("Sent a reply");
+        #ifdef DEBUG
+          Serial.println("Reply message sent");
+        #endif
       }
-    } else {
-      Serial.println("Receive failed");
+      else 
+      {
+        #ifdef DEBUG
+          Serial.println("Receive failed");
+        #endif
+        #ifdef EXT_DEBUG
+          displayMessage(0,"No message received");
+        #endif
+      }
     }
   }
-}
+} 
+
+#ifdef EXT_DEBUG
+  void displayMessage(int row, String message)
+  // first row is zero
+  {
+    lcd.setCursor(0, row);
+    lcd.print(message);
+  }
+#endif
